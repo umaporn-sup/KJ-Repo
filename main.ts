6ec2d58b35d1e15fcf4de3ebce4bb04d8aae46d8
Khajonsak Chalermdit <35735538+KChalermdit@users.noreply.github.com>
2020-05-22 14:36:06 +0700

    we have item change by B button

--- main.ts
+++ main.ts
@@ -7,6 +7,8 @@ namespace SpriteKind {
     export const Placed = SpriteKind.create()
     export const Cursor = SpriteKind.create()
     export const Box = SpriteKind.create()
+    export const Item = SpriteKind.create()
+    export const Icon = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -48,53 +50,47 @@ f f f f f f f f f f f e e f f f
 f f f f f f f f f f f f f f f f 
 `
 }
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    Create_Object(tree, point.x, point.y)
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Create_Object(house, point.x, point.y)
-})
-function Remove_Object (Object: Sprite) {
-    Object.destroy()
-    point.say("Remove", 500)
-    music.pewPew.play()
-}
-sprites.onOverlap(SpriteKind.Cursor, SpriteKind.Placed, function (sprite, otherSprite) {
-    if (controller.left.isPressed() && controller.right.isPressed()) {
-        Remove_Object(otherSprite)
-    }
-})
-function Create_Object (Object: Sprite, positionx: number, positiony: number) {
-    House_Placed = sprites.create(Object.image, SpriteKind.Placed)
+function Create_Object (Object2: Sprite, positionx: number, positiony: number) {
+    House_Placed = sprites.create(Object2.image, SpriteKind.Placed)
     House_Placed.setPosition(positionx, positiony)
-    point.say(sprites.readDataString(Object, "name"), 500)
+    popup.say(sprites.readDataString(Object2, "name"), 500)
     music.magicWand.play()
 }
-let House_Placed: Sprite = null
-let tree: Sprite = null
-let house: Sprite = null
-let point: Sprite = null
-blockSettings.writeString("villageName", "Thawin")
-game.showLongText("Move Cursor   then press A = Create House,  B = Tree,      Left+Right(on Object) =     Remove Object", DialogLayout.Center)
-let villageBanner = sprites.create(img`
+function nextItem2 () {
+    nowItem += 1
+    if (nowItem > listItem.length - 1) {
+        nowItem = 0
+    }
+}
+function Init_Game () {
+    villageBanner = sprites.create(img`
 5 5 5 5 5 5 5 5 5 4 
 5 f f f f f f f 5 4 
 5 5 5 5 5 5 5 5 5 4 
 4 4 4 4 4 4 4 4 4 4 
 `, SpriteKind.Player)
-villageBanner.setPosition(50, 18)
-sprites.setDataString(villageBanner, "villageName", blockSettings.readString("villageName"))
-villageBanner.say("" + sprites.readDataString(villageBanner, "villageName") + " Village")
-point = sprites.create(img`
+    villageBanner.setPosition(50, 18)
+    sprites.setDataString(villageBanner, "villageName", blockSettings.readString("villageName"))
+    villageBanner.say("" + sprites.readDataString(villageBanner, "villageName") + " Village")
+    pointer = sprites.create(img`
 3 3 . 3 3 
 3 . . . 3 
 . . 3 . . 
 3 . . . 3 
 3 3 . 3 3 
 `, SpriteKind.Cursor)
-controller.moveSprite(point)
-point.z = 255
-house = sprites.create(img`
+    controller.moveSprite(pointer)
+    pointer.z = 255
+    popup = sprites.create(img`
+3 3 . 3 3 
+3 . . . 3 
+. . 3 . . 
+3 . . . 3 
+3 3 . 3 3 
+`, SpriteKind.Cursor)
+    popup.z = 254
+    controller.moveSprite(popup)
+    house = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . 8 a 8 a a 8 a 8 . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . a a a 8 8 8 a a 8 a 8 a a a . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . a a a 8 a a 8 a 8 a a 8 8 8 a a 8 a a a . . . . . . . . . . . . . . 
@@ -143,10 +139,10 @@ c c b b b b b b b b b b d c b b e 3 b 3 3 b 3 3 b 3 3 f e e e e b b c d b b b b 
 . . . . b d d d d d d d d c b b f 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c d d d d d d d d b . . . . 
 . . . . . b d b b b d d d c b b f 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c d d d b b b d b . . . . . 
 . . . . . . b c c c b b b c b b e 3 b 3 3 b 3 3 b 3 3 b 3 3 b e b b c b b b c c c b . . . . . . 
-`, SpriteKind.Placed)
-sprites.setDataString(house, "name", "House")
-house.setFlag(SpriteFlag.Invisible, true)
-tree = sprites.create(img`
+`, SpriteKind.Item)
+    sprites.setDataString(house, "name", "House")
+    house.setFlag(SpriteFlag.Invisible, true)
+    tree = sprites.create(img`
 . . . . . . . . . . . . . . . . 8 6 . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . 6 6 8 8 8 6 7 8 8 6 . . . . . . . . . . . . . . . 
 . . . . . . . . . . . 8 6 6 6 8 7 7 6 8 8 8 6 8 . . . . . . . . . . . . 
@@ -188,10 +184,52 @@ tree = sprites.create(img`
 . . . . . . . . . . . . . . . f e e f c e . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . f c e e e c . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . f f c e e c . . . . . . . . . . . . . . . 
-`, SpriteKind.Placed)
-sprites.setDataString(tree, "name", "Tree")
-tree.setFlag(SpriteFlag.Invisible, true)
-tiles.setTilemap(tiles.createTilemap(
+`, SpriteKind.Item)
+    sprites.setDataString(tree, "name", "Tree")
+    tree.setFlag(SpriteFlag.Invisible, true)
+    bush = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . c c c c 6 . . . . . 
+. . . . c c 6 7 7 5 5 6 6 . . . 
+. . c c 6 6 6 6 7 5 5 7 c c . . 
+. c 6 6 6 7 7 7 7 7 7 5 6 c c . 
+. c 6 6 7 7 7 5 7 6 7 7 7 6 c c 
+c 6 6 7 7 6 7 7 7 6 7 7 6 6 6 c 
+c c 6 6 6 7 6 7 6 6 6 6 5 7 6 c 
+c c c c 6 7 7 6 7 7 7 6 7 6 6 c 
+. c c 6 6 6 6 c 6 6 6 6 6 c c c 
+. c c 6 6 c 6 6 c 6 c 6 6 c c . 
+. . c c f f 6 6 c f f c c f . . 
+. . . . c f c c c f c f f . . . 
+. . . . . 4 f f f c . e . . . . 
+. . . . . . e e e . . 4 . . . . 
+. . . . . . . e e . e . . . . . 
+`, SpriteKind.Item)
+    sprites.setDataString(bush, "name", "Bush")
+    bush.setFlag(SpriteFlag.Invisible, true)
+    flower = sprites.create(img`
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. b b d d b b . 
+b 1 1 3 3 1 1 b 
+b 1 3 5 5 3 1 b 
+b d 3 5 5 3 d b 
+c 1 1 3 d 1 1 c 
+c d 1 d d 1 d c 
+. c c 7 6 c c . 
+. . 6 7 6 . . . 
+. . 6 6 8 8 8 6 
+. . 6 8 7 7 7 6 
+. . 8 7 7 7 6 . 
+. . 8 8 8 6 . . 
+`, SpriteKind.Item)
+    sprites.setDataString(flower, "name", "Flower")
+    flower.setFlag(SpriteFlag.Invisible, true)
+    listItem = sprites.allOfKind(SpriteKind.Item)
+    nowItem = 0
+    tiles.setTilemap(tiles.createTilemap(
             hex`1000100003030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`,
             img`
 2 2 2 2 2 2 . . . . . . . . . . 
@@ -214,4 +252,37 @@ tiles.setTilemap(tiles.createTilemap(
             [myTiles.tile0,myTiles.tile1,sprites.castle.tileGrass1,sprites.castle.tileDarkGrass1,sprites.castle.tileGrass3,sprites.castle.tileDarkGrass2],
             TileScale.Sixteen
         ))
-scene.cameraFollowSprite(point)
+    scene.cameraFollowSprite(pointer)
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    nextItem2()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Create_Object(listItem[nowItem], pointer.x, pointer.y)
+})
+function Remove_Object (Object2: Sprite) {
+    Object2.destroy()
+    popup.say("Remove", 500)
+    music.pewPew.play()
+}
+sprites.onOverlap(SpriteKind.Cursor, SpriteKind.Placed, function (sprite, otherSprite) {
+    if (controller.left.isPressed() && controller.right.isPressed()) {
+        Remove_Object(otherSprite)
+    }
+})
+let flower: Sprite = null
+let bush: Sprite = null
+let tree: Sprite = null
+let house: Sprite = null
+let pointer: Sprite = null
+let villageBanner: Sprite = null
+let listItem: Sprite[] = []
+let nowItem = 0
+let popup: Sprite = null
+let House_Placed: Sprite = null
+blockSettings.writeString("villageName", "Thawin")
+game.showLongText("Move Cursor   then press A = Create Item,   B = Change Next Item, Left+Right(on Object) = Remove Object", DialogLayout.Center)
+Init_Game()
+game.onUpdate(function () {
+    pointer.say("*" + sprites.readDataString(listItem[nowItem], "name"))
+})
